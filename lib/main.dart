3b63134cd1fb0ea4aa3c17d386b3170f7e87f856
ramabie <rama.biee@gmail.com>
2025-01-16 20:26:49 +0700

    perubahan kedua

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Selamat Datang'),
+      home: const MyHomePage(title: 'Selamat Datang Flutter'),
     );
   }
 }
